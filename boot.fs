21e7ce07001a9c605d405d40e9f219c964386c94
Marcos Cruz <github@programandala.net>
2016-01-02 03:02:00 +0100

    Update 2016-01-02T03:02.

--- boot.fs
+++ boot.fs
@@ -1,7 +1,7 @@
 ( boot.fs )
 
 ( This file is part of Sfera, a library for QL SUPERFORTH )
-( Version: 0.0.0+201601010525 )
+( Version: 0.0.0+201601020225 )
 ( http://programandala.net/en.program.sfera.html )
 
 ( Author: Marcos Cruz [programandala.net] )
@@ -21,33 +21,86 @@
 ( http://programandala.net/en.program.sfera.history.html )
 
 ( ============================================================== )
-( Basic extensions )
 
-LOWER
+LOWER  ( case-insensitive mode )
 
-( ---------------------------------------------- )
+: noop  ( -- )  ;
+
+( ============================================================== )
 ( Comments )
 
 : \  ( "ccc<eol>" -- )
   10 word drop  ;  immediate
+  \ Ignore the rest of the input stream line.
 
-\ ----------------------------------------------
-\ Address artithmetic
+: ?\  ( f "ccc<eol>" -- )
+  if  [compile] \
+  then  ; immediate
+  \ If _f_ is not zero, ignore the rest of the input stream line
 
-: cell+  ( a1 -- a2 )  2+  ;
-: cell-  ( a1 -- a2 )  2-  ;
-: cells  ( a1 -- a2 )  2*  ;
+\  ;
+\ : Stack
+
+: nip  ( x1 x2 -- x2 )  swap drop  ;
+
+: tuck  ( x1 x2 -- x2 x1 x2 )  swap over  ;
+
+: bounds  ( ca len -- ca2 ca )  over + swap  ;
+
+\ ============================================================ ;
+\ : Text output
+
+: home  ( -- )  0 0 at  ;
+
+\ ============================================================ ;
+\ : : Parsing
+
+: parse-word  ( "name" -- ca )  bl word  ;
+
+: parse-name  ( "name" -- ca len )  parse-word count  ;
+
+: defined  ( "name" -- ca 0 | cfa 1 | cfa -1 )  parse-word find  ;
+
+: [undefined]  ( "name" -- f )  defined nip 0=  ; immediate
+: [defined]  ( "name" -- f )  [compile] [undefined] 0=  ; immediate
+
+\ ============================================================ ;
+\ : Compiling
+
+: compile,  ( cfa -- )  ,  ;
+
+: postpone  ( "name" -- )
+  defined dup 0= 0 ?error  (  cfa 1 | cfa -1 )
+  -1 = if    compile compile  \ non-immediate
+       then  compile,
+  ; immediate
+
+
+: executing?  ( -- f )  state @ 0=  ;
+: compiling?  ( -- f )  executing? 0=  ;
+
+\ ============================================================ ;
+\ : Address artithmetic
+
+: cell+  ( a1 -- a2 )
+  compiling? if  postpone 2+  else  2+  then  ; immediate
+
+: cell-  ( a1 -- a2 )
+  compiling? if  postpone 2-  else  2-  then  ; immediate
+
+: cells  ( a1 -- a2 )
+  compiling? if  postpone 2*  else  2*  then  ; immediate
 
 : chars  ( ca1 -- ca2 )  ; immediate
 
-\ ----------------------------------------------
-\ Stack
+: char+  ( ca1 -- ca2 )
+  compiling? if  postpone 1+  else  1+  then  ; immediate
 
-: nip  ( x1 x2 -- x2 )  swap drop  ;
-: bounds  ( ca len -- ca2 ca )  over + swap  ;
+: char-  ( ca1 -- ca2 )
+  compiling? if  postpone 1-  else  1-  then  ; immediate
 
-\ ----------------------------------------------
-\ Operators
+\ ============================================================ ;
+\ : Operators
 
 : 0<>  ( x -- f )  0 <>  ;
 
@@ -59,21 +112,24 @@ LOWER
 : on  ( a -- )  true swap !  ;
 : off  ( a -- )  false swap !  ;
 
-\ ----------------------------------------------
-\ Word headers
+\ ============================================================ ;
+\ : Word headers
 
-: traverse  ( a1 n -- a2 )
+: traverse  ( ca1 n -- ca2 )
   swap  begin  over + 127 over c@ <  until
   swap drop  ;
-  \ Move across a name field.  _a1_ is the address of either the
+  \ Move across a name field.  _ca1_ is the address of either the
   \ length byte or the last letter.  If _n_=1, the motion is
   \ toward hi memory; if _n_=-l, the motion is toward low
-  \ memory.  The _a2_ resulting is the address of the other end
+  \ memory.  _ca2_ is the address of the other end
   \ of the name.
   \
   \ Origin: fig-Forth.
 
-: >name       ( cfa -- nfa )  1- -1 traverse  ;
+: >name       ( cfa -- nfa )
+  1-  dup c@ bl = +  \ skip a possible padding space
+  -1 traverse  ;
+
 : name>       ( nfa -- cfa )  1 traverse 1+  ;
 : name>link   ( nfa -- lfa )  cell-  ;
 : name>body   ( nfa -- pfa )  name> >body  ;
@@ -84,37 +140,64 @@ LOWER
 : body>name   ( pfa -- nfa )  body> >name  ;
 : body>link   ( pfa -- lfa )  body>name name>link  ;
 
-: executing?  ( -- f )  state @ 0=  ;
-: compiling?  ( -- f )  executing? 0=  ;
-
 64 constant immediate-mask
 : immediate?  ( cfa -- f )  >name c@ immediate-mask and  ;
 
-\ ----------------------------------------------
-\ Parsing
+31 constant /name
+  \ maximum length of a word name, also used as bitmask
 
-: parse-word  ( "name" -- ca )  bl word  ;
+: name>length  ( nfa -- len )
+  dup >r c@ /name and dup
+  r> dup 1 traverse - abs <> abort" invalid name field"
+                      dup 0= abort" name empty"  ;
 
-: parse-name  ( "name" -- ca len )  parse-word count  ;
+create name /name 1+ allot
+0 name c!  name 1+ /name blank
+  \ Name buffer.
 
-: defined  ( "name" -- ca 0 | cfa 1 | cfa -1 )  parse-word find  ;
+: clean-name-bound  ( ca -- )  dup c@ 127 and swap c!  ;
+  \ Remove bit 7 from _ca_.
 
-: [undefined]  ( "name" -- f )  defined nip 0=  ; immediate
-: [defined]  ( "name" -- f )  [compile] [undefined] 0=  ; immediate
+: clean-name  ( -- )
+  name dup dup 1+ clean-name-bound
+             c@ + clean-name-bound  ;
+  \ Remove bit 7 from the bounds of the name stored in the name buffer.
 
-\ ----------------------------------------------
-\ Compiling
+: name>string  ( nfa -- ca len )
+  dup name>length dup name c! >r
+  1+ name 1+ r@ cmove  clean-name  name 1+ r>  ;
 
-: compile,  ( cfa -- )  ,  ;
+: .name  ( nfa -- )
+  name>string type  ;
 
-: postpone  ( "name" -- )
-  defined dup 0= 0 ?error  (  cfa 1 | cfa -1 )
-  -1 = if    compile compile  \ non-immediate
-       then  compile,
-  ; immediate
+\ ============================================================ ;
+\ : Number prefixes
 
-\ ----------------------------------------------
-\ Number prefixes
+exvec: adjust-number  ( d -- d | n )
+
+: single-number-prefix  ( -- )
+  assign adjust-number to-do drop  ;
+
+: double-number-prefix  ( -- )
+  assign adjust-number to-do noop  ;
+
+: numeric-prefix  ( b "name" -- )
+  create c, immediate
+  does>  ( "name" -- d | n ) ( pfa )
+    base @ >r  c@ base !
+    parse-word number adjust-number postpone literal
+    r> base !  ;
+
+: 2numeric-prefix  ( b "name" -- )
+  double-number-prefix numeric-prefix single-number-prefix  ;
+
+ 2 numeric-prefix b# ( "name" -- n )
+10 numeric-prefix d# ( "name" -- n )
+16 numeric-prefix h# ( "name" -- n )
+
+ 2 2numeric-prefix 2b# ( "name" -- d )
+10 2numeric-prefix 2d# ( "name" -- d )
+16 2numeric-prefix 2h# ( "name" -- d )
 
 : c#  ( "name" -- c )
   parse-name drop c@
@@ -122,36 +205,92 @@ LOWER
   \ Parse a name and return the code of the its first
   \ character. This is an alternative to the standard words
   \ `char` and `[char]`.
-  \
-  \ Credits:
-  \
-  \ Code from the Solo Forth library.
-  \ Original code inspired by eForth.
 
-\ ----------------------------------------------
-\ Strings
+\ ============================================================ ;
+\ : Number output
+
+: binary  ( -- )  2 base !  ;
+
+  \ XXX OLD -- SUPERFORTH has `h.`
+  \ : hex.  ( n -- )  base @ >r  hex u. r> base !  ;
+
+: bin.  ( n -- )  base @ >r  binary u. r> base !  ;
+
+: (d.)  ( d n -- ca len )  <# 0 do  #  loop  #>  ;
+
+variable base'
+
+: <hex  ( -- )  base @ base' ! hex  ; \ switch to hex
+: hex>  ( -- )  base' @ base !      ; \ and back
+
+: (dhex.)  ( d n -- )  <hex (d.) hex> type space  ;
+
+: 32hex.   ( d -- ) 8 (dhex.)  ;
+: 16hex.   ( n -- ) s->d 4 (dhex.)  ;
+: 8hex.    ( b -- ) s->d 2 (dhex.)  ;
+
+: <bin  ( -- )  base @ base' ! binary  ; \ switch to binary
+: bin>  ( -- )  base' @ base !         ; \ and back
+
+: (dbin.)  ( d n -- )  <bin (d.) bin> type space  ;
+
+: 32bin.   ( d -- ) 32 (dbin.)  ;
+: 16bin.   ( n -- ) s->d 16 (dbin.)  ;
+: 8bin.    ( b -- ) s->d 8 (dbin.)  ;
+
+\ ============================================================ ;
+\ : Control structures
+
+: again  ( -- )  postpone 0 postpone until  ; immediate
+
+\ ============================================================ ;
+\ : Strings
 
 : s"  ( "ccc<quote>" --- ca len )
   c# " word dup count pad swap cmove c@ pad swap ;
 
-\ ==============================================================
-\ Keyboard
+\ ============================================================ ;
+\ : Keyboard
 
-: shift-key?  ( -- f )  7 keyrow 1 and 0<>  ;
-: space-key?  ( -- f )  1 keyrow 64 and 0<>  ;
-: break-key?  ( -- f )  shift-key? space-key? and  ;
+: .keyrow  ( n -- )
+  0 over at dup . keyrow 8bin.  ;
+  \ Print keyrow _n_.
+
+: .keyrows  ( n -- )
+  s->d time d+
+  begin
+    8 0 do  i .keyrow  loop  2dup time d<
+  until  2drop  ;
+  \ Print the keyboard matrix during _n_ seconds.
+
+: control-key?  ( -- f )  7 keyrow b# 00000010 and 0<>  ;
+: shift-key?    ( -- f )  7 keyrow b# 00000001 and 0<>  ;
+: alt-key?      ( -- f )  7 keyrow b# 00000100 and 0<>  ;
+: space-key?    ( -- f )  1 keyrow b# 01000000 and 0<>  ;
+: escape-key?   ( -- f )  1 keyrow b# 00001000 and 0<>  ;
+: enter-key?    ( -- f )  1 keyrow b# 00000001 and 0<>  ;
+: tab-key?      ( -- f )  5 keyrow b# 00001000 and 0<>  ;
+
+: break-key?  ( -- f )  control-key? space-key? and  ;
 
 : key?  ( -- f )
   0  8 0 do  i keyrow or  loop  0<>  ;
 
+  \ XXX FIXME -- never returns, why?
+: no-key  ( -- )  begin  key? 0=  until  ;
+
+: timeout!  ( n -- )  [ ' timeout >body ] literal !  ;
+  \ Store _n_ into the constant `timeout`.
+
   \ XXX FIXME
 : inkey  ( -- c )
-  timeout >r
-   1 [ ' timeout >body ] literal !  key
-  r> [ ' timeout >body ] literal !  ;
+  timeout >r  1 timeout! key  r> timeout!  ;
+  \ Key the pressed key, if any (without waiting for a key
+  \ press).
 
+\ XXX OLD
 : aborted?  ( c -- f )
-  key? if  key =  else  drop false  then  ;
+  key? if  no-key key =  else  drop false  then  ;
   \ If no key is pressed return _false_.  If a key is pressed,
   \ discard it and wait for a second key. Ther return _true_ if
   \ it's _c_, else return _false_.
@@ -161,60 +300,98 @@ LOWER
   \ : listing  ( -- )
   \   begin  ." bla "  bl aborted?  until  ." Aborted"  ;
 
+\ XXX OLD
 10 constant 'cr' \ code of carriage return
 
 : nuf?  ( -- f )  'cr' aborted?  ;
 
-252 constant break-key
+: wait-space-key-pressed  ( -- )  begin  space-key? 0=  until  ;
 
-  \ XXX FIXME -- never returns, why?
-: no-key  ( -- )  begin  key? 0=  until  ;
+: wait-space-key-released  ( -- )  begin  space-key?  until  ;
 
-: enough?  ( -- f )
-  break-key? if  no-key key break-key =  else  false  then  ;
+: space-paused  ( -- )
+  space-key? if
+    wait-space-key-pressed
+    wait-space-key-released
+    wait-space-key-pressed
+  then  ;
 
-\ ==============================================================
-\ Tools
+: wait-enter-key-pressed  ( -- )  begin  enter-key? 0=  until  ;
 
-: binary  ( -- )  2 base !  ;
-: hex.  ( n -- )  base @ >r  hex u. r> base !  ;
-: bin.  ( n -- )  base @ >r  binary u. r> base !  ;
+: wait-enter-key-released  ( -- )  begin  enter-key?  until  ;
 
-: (d.)  ( d n -- ca len )  <# 0 do  #  loop  #>  ;
+: enter-paused  ( -- )
+  enter-key? if
+    wait-enter-key-pressed
+    wait-enter-key-released
+    wait-enter-key-pressed
+  then  ;
 
-variable base'
-
-: <hex  ( -- )  base @ base' ! hex ; \ switch to hex
-: hex>  ( -- )  base' @ base !     ; \ and back
-
-: (dhex.)  ( d n -- )  <hex (d.) hex> type space  ;
-
-: 32hex.   ( d -- ) 8 (dhex.)  ;
-: 16hex.   ( n -- ) s->d 4 (dhex.)  ;
-: 8hex.    ( b -- ) s->d 2 (dhex.)  ;
+\ ============================================================ ;
+\ : Tools
 
 : first-word?  ( lfa1 -- lfa2 f )  @ dup 32768 =  ;
 
 : words  ( -- )
   latest ( lfa )
   begin
-    dup id. space
-    first-word?  enough? or
+    dup id. space space-paused
+    first-word?  escape-key? or
   until  drop  ;
 
+: |  ( -- )  ." | "  ;
+: table  ( -- )  cr ." |==="  ;
+
+: .xheader-field  ( n -- )  | 16hex. ;
+
+: .xtype  ( cfa -- )
+  dup @ over >body = if  drop ." CODE      " exit  then
+     dup @ h# 87C6 = if  drop ." :         " exit  then
+     dup @ h# 879A = if  drop ." CREATE    " exit  then
+     dup @ h# 96D0 = if  drop ." 2CONSTANT " exit  then
+     dup @ h# 877C = if  drop ." CONSTANT  " exit  then
+         @ h# 9542 = if       ." EXVECT:   " exit  then
+  ." unknown   "  ; 
+
+: .xheader  ( lfa -- )
+  cr dup   .xheader-field  dup link> >r
+  r@ >name .xheader-field
+  r@       .xheader-field
+  r@ @     .xheader-field space
+  r@ >body .xheader-field
+  r>       | .xtype
+           | id. ;
+
 : xwords  ( -- )
+  table
+  cr ." | LFA  | NFA  | CFA  | Code | PFA  | Type      | Name" cr
   latest ( lfa )
   begin
-    dup link> 16hex. dup id. space
-    first-word?  enough? or
-  until  drop  ;
+    dup .xheader space-paused
+    first-word?  escape-key? or
+  until  drop  table  ;
+
+\ File access methods
+0 constant old-exclusive
+1 constant old-shared
+2 constant new-exclusive
+3 constant directory
+
+status nfa4_printout.txt ( ior )
+\ file does not exist: ior = 0
+\ file already exist: ior = -8 (other errors are ignored)
+0= abs 2* \ calculate file access method
+open nfa4_printout.txt #print 2!
+
+: xwords>printer  ( -- )
+  printer_on xwords printer_off  ;
 
   \ XXX TODO
 : find-cfa  ( cfa -- )
   latest ( lfa )
   begin
-    dup link> 16hex. dup id. space
-    first-word?  enough? or
+    dup link> 16hex. dup id. space space-paused
+    first-word?  escape-key? or
   until  drop  ;
 
 : .n  ( cfa -- )  >link id.  ;
@@ -245,8 +422,8 @@ variable base'
   ?dup if  (wdump)  else  drop  then  ;
   \ Show the contents of _n_ cells starting from _a_.
 
-\ ==============================================================
-\ Windows
+\ ============================================================ ;
+\ : Windows
 
 \ Note: The main window can not be changed during interpretation
 \ of a file, because `#in` is used to redirect the file being
@@ -288,8 +465,8 @@ default-colors cls
   \ Create a word called "name" that, when executed,
   \ will set channel _d_ as the main window.
 
-0 open con_800x600a0x0  2dup 2constant #800x600
-0 open con_1280x800a0x0 2dup 2constant #1280x800
+0 open con_800x600a0x0  2constant #800x600
+0 open con_1280x800a0x0 2constant #1280x800
 
 : w800  ( -- )  #800x600 set-main-window  ;
 : w1280 ( -- )  #1280x800 set-main-window  ;
@@ -299,8 +476,8 @@ cr
 .(   w800  = 800x600)  cr
 .(   w1280 = 1280x800)  cr
 
-\ ==============================================================
-\ Devices
+\ ============================================================ ;
+\ : Devices
 
 : create_device  ( "name" -- )
 
@@ -310,10 +487,10 @@ cr
   \ device called fdr1_ as the default
 
   create
-    latest 5 + @      \ get characters 3 and 4
-    dup 63 and 48 - , \ compile device number
-    ,                 \ compile characters 3 and 4
-    latest 3 + @ ,    \ compile first 2 characters
+    latest 5 + @        \ get characters 3 and 4
+    dup 63 and c# 0 - , \ compile device number
+    ,                   \ compile characters 3 and 4
+    latest 3 + @ ,      \ compile first 2 characters
   does>  ( -- ) ( pfa )
     >r r@ @
     r@ cell+ @
@@ -327,8 +504,8 @@ create_device nfa1_
 create_device nfa4_
 nfa4_
 
-\ ==============================================================
-\ Files
+\ ============================================================ ;
+\ : Files
 
 : include  ( "name" -- )
   cr .s key drop \ XXX INFORMER
@@ -349,47 +526,13 @@ nfa4_
   cr .s key drop \ XXX INFORMER
   ;
 
-\ ==============================================================
-\ Number prefixes
-
-  \ Credits:
-  \
-  \ Code modified from the Solo Forth library.
-  \ Original code adapted from eForth.
-
-exvec: adjust-number  ( d -- d | n )
-
-: single-number-prefix  ( -- )
-  assign adjust-number to-do drop  ;
-
-: double-number-prefix  ( -- )
-  assign adjust-number to-do noop  ;
-
-: numeric-prefix  ( b "name" -- )
-  create c, immediate
-  does>  ( "name" -- d | n ) ( pfa )
-    base @ >r  c@ base !
-    parse-word number adjust-number postpone literal
-    r> base !  ;
-
-: 2numeric-prefix  ( b "name" -- )
-  double-number-prefix numeric-prefix single-number-prefix  ;
-
- 2 numeric-prefix b# ( "name" -- n )
-10 numeric-prefix d# ( "name" -- n )
-16 numeric-prefix h# ( "name" -- n )
-
- 2 2numeric-prefix 2b# ( "name" -- d )
-10 2numeric-prefix 2d# ( "name" -- d )
-16 2numeric-prefix 2h# ( "name" -- d )
-
-\ ==============================================================
-\ Decode
+\ ============================================================ ;
+\ : Decode
 
   \ Adapted from Solo Forth
   \ http://programandala.net/en.program.solo_forth.html
 
-forth definitions decimal hex
+forth definitions decimal
 
 variable decode-level  decode-level off
   \ depth of nesting
@@ -397,18 +540,18 @@ variable decode-level  decode-level off
 variable decode-address
   \ in the word being decoded
 
-83AC constant (loop)-cfa
-83B8 constant (+loop)-cfa
-8824 constant lit-cfa
-87C6 constant do-colon
+h# 83AC constant (loop)-cfa
+h# 83B8 constant (+loop)-cfa
+h# 8824 constant lit-cfa
+h# 87C6 constant do-colon
 
 \ ----------------------------------------------
 \ Printing
 
 : indent  ( -- )
-  cr decode-address @ u. decode-level @ 02 * spaces  ;
+  cr decode-address @ u. decode-level @ 2 * spaces  ;
 
-: indent+  ( -- )  01 decode-level +! indent  ;
+: indent+  ( -- )  1 decode-level +! indent  ;
 
 \ ----------------------------------------------
 \ Special cases
@@ -465,7 +608,7 @@ variable decode-address
                   bl  of  drop      endof \ space
                              swap recurse \ default
           endcase  decode-special
-      cell+  -01 decode-level +!
+      cell+  -1 decode-level +!
     repeat  indent cell+ body>link id. \ show the last word
   else  ." Not a colon definition."
   then  drop  ;
@@ -479,15 +622,13 @@ variable decode-address
   cr ." Keys: space=more, q=quit, other=deeper." cr  ;
 
 : decode  ( "name" -- )
-  defined 0= 00 ?error
+  defined 0= 0 ?error
   decode-usage  decode-level off  >body (decode)  ;
-
-decimal
 
 end_file \ XXX TMP
 
-\ ==============================================================
-\ XXX UNDER DEVELOPMENT
+\ ============================================================ ;
+\ : XXX UNDER DEVELOPMENT
 
 \ include nfa4_words_fth
 
@@ -519,8 +660,8 @@ end_file \ XXX TMP
 
 : [then]  ( -- )  ; immediate
 
-\ ==============================================================
-\ Error
+\ ============================================================ ;
+\ : Error
 
 variable catcher
 
